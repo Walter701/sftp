04a4e0621a744217bd04d6a16742afd65d433f0a
Walter701 <98479706+Walter701@users.noreply.github.com>
2023-01-03 12:40:43 -0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,24 +10,6 @@ echo $FOLDER
 # running this init script.
 if [ "$1" == '/usr/sbin/sshd' ]; then
 
-  # Ensure time is in sync with host
-  # see https://wiki.alpinelinux.org/wiki/Setting_the_timezone
-  if [ -n ${TZ} ] && [ -f /usr/share/zoneinfo/${TZ} ]; then
-    ln -sf /usr/share/zoneinfo/${TZ} /etc/localtime
-    echo ${TZ} > /etc/timezone
-  fi
-
-  # Regenerate keys
-  if [ ! -f "/etc/ssh/ssh_host_rsa_key" ]; then
-    ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
-  fi
-  if [ ! -f "/etc/ssh/ssh_host_dsa_key" ]; then
-    ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
-  fi
-  if [ ! -f "/etc/ssh/ssh_host_ecdsa_key" ]; then
-    ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa
-  fi
-
   useradd -m $USERNAME -g sftp -d $FOLDER/$USERNAME
 
   # Change sftp password
