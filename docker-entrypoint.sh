1c6bc9043af465631b5dbe519f5606133cc779c1
Walter701 <98479706+Walter701@users.noreply.github.com>
2023-01-03 14:42:26 -0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,8 +4,18 @@ set -e
 # Allow to run complementary processes or to enter the container without
 # running this init script.
 if [ "$1" == '/usr/sbin/sshd' ]; then
+  mkdir -p /sftp/etc/ssh
+  if [ ! -f /sftp/etc/ssh/ssh_host_rsa_key ]; then
+    ssh-keygen -f /sftp/etc/ssh/ssh_host_rsa_key -N '' -t rsa
+  fi
+  if [ ! -f /sftp/etc/ssh/ssh_host_dsa_key ]; then
+    ssh-keygen -f /sftp/etc/ssh/ssh_host_dsa_key -N '' -t dsa
+  fi
+  if [ ! -f /sftp/etc/ssh/ssh_host_ecdsa_key ]; then
+    ssh-keygen -f /sftp/etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa
+  fi
+  cp /etc/ssh/sshd_config /sftp/etc/ssh/
 
-  # Mount the data folder in the chroot folder
 
   # Check if a script is available in /docker-entrypoint.d and source it
   # You can use it for example to create additional sftp users
