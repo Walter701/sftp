73f44aa2fc5af8eb4358d8842f5dd75d8b38f4bd
Walter701 <98479706+Walter701@users.noreply.github.com>
2023-01-03 12:28:26 -0300

    Commit inicial

---
+++ docker-entrypoint.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -e
+
+FOLDER="/sftp"
+USERNAME="sftpdevelop"
+PASSWORD="1q2w3e4r"
+mkdir $FOLDER
+echo $FOLDER
+
+# Allow to run complementary processes or to enter the container without
+# running this init script.
+if [ "$1" == '/usr/sbin/sshd' ]; then
+
+  # Ensure time is in sync with host
+  # see https://wiki.alpinelinux.org/wiki/Setting_the_timezone
+  if [ -n ${TZ} ] && [ -f /usr/share/zoneinfo/${TZ} ]; then
+    ln -sf /usr/share/zoneinfo/${TZ} /etc/localtime
+    echo ${TZ} > /etc/timezone
+  fi
+
+  # Regenerate keys
+  if [ ! -f "/etc/ssh/ssh_host_rsa_key" ]; then
+    ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+  fi
+  if [ ! -f "/etc/ssh/ssh_host_dsa_key" ]; then
+    ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
+  fi
+  if [ ! -f "/etc/ssh/ssh_host_ecdsa_key" ]; then
+    ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa
+  fi
+
+  useradd -m $USERNAME -g sftp -d $FOLDER/$USERNAME
+
+  # Change sftp password
+  echo "$USERNAME:$PASSWORD" | chpasswd
+
+  # Mount the data folder in the chroot folder
+
+  # Check if a script is available in /docker-entrypoint.d and source it
+  # You can use it for example to create additional sftp users
+  for f in /docker-entrypoint.d/*; do
+    case "$f" in
+      *.sh)  echo "$0: running $f"; . "$f" ;;
+      *)     echo "$0: ignoring $f" ;;
+    esac
+  done
+
+fi
+
+exec "$@"
+
+
