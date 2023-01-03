dc7def5da194716cbb16351c63e30da20b3eb72a
Walter701 <98479706+Walter701@users.noreply.github.com>
2023-01-03 12:51:14 -0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 set -e
 
-FOLDER="/sftp"
-USERNAME="sftpdevelop"
-PASSWORD="1q2w3e4r"
-echo $FOLDER
-
 # Allow to run complementary processes or to enter the container without
 # running this init script.
 if [ "$1" == '/usr/sbin/sshd' ]; then
-
-  useradd -m $USERNAME -g sftp -d $FOLDER/$USERNAME
-
-  # Change sftp password
-  echo "$USERNAME:$PASSWORD" | chpasswd
 
   # Mount the data folder in the chroot folder
 
@@ -29,5 +19,3 @@ if [ "$1" == '/usr/sbin/sshd' ]; then
 fi
 
 exec "$@"
-
-
